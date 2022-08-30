bb3c6a41d62ab816e49e0615d8547a1e14a91841
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-08-30 16:50:42 +0200

    Running the algorithm works, but spawns a view and is a bit slow

--- playground/hello_gym_auv.py
+++ playground/hello_gym_auv.py
@@ -7,47 +7,64 @@
 import gym_auv
 import gym
 env_name = "TestScenario1-v0" 
-env_config = gym_auv.SCENARIOS[env_name]['config']
+env_config = gym_auv.SCENARIOS[env_name]['config'].copy()
+print(f"{env_config = }")
 env = gym.make(env_name, env_config=env_config)
+# Import the RL algorithm (Algorithm) we would like to use.
+from ray.rllib.algorithms.ppo import PPO
+from ray import tune
 
+from gym_auv.envs.testscenario import TestScenario1
 
-# # Import the RL algorithm (Algorithm) we would like to use.
-# from ray.rllib.algorithms.ppo import PPO
+env.reset()
+obs = env.step([0.5, 0.5])
+print(obs)
 
-# # Configure the algorithm.
-# config = {
-#     # Environment (RLlib understands openAI gym registered strings).
-#     "env": "Taxi-v3",
-#     # Use 2 environment workers (aka "rollout workers") that parallelly
-#     # collect samples from their own environment clone(s).
-#     "num_workers": 2,
-#     # Change this to "framework: torch", if you are using PyTorch.
-#     # Also, use "framework: tf2" for tf2.x eager execution.
-#     "framework": "tf",
-#     # Tweak the default model provided automatically by RLlib,
-#     # given the environment's observation- and action spaces.
-#     "model": {
-#         "fcnet_hiddens": [64, 64],
-#         "fcnet_activation": "relu",
-#     },
-#     # Set up a separate evaluation worker set for the
-#     # `algo.evaluate()` call after training (see below).
-#     "evaluation_num_workers": 1,
-#     # Only for evaluation runs, render the env.
-#     "evaluation_config": {
-#         "render_env": True,
-#     },
-# }
+def train():
+    # Register environment in RLlib
+    tune.register_env(env_name, lambda env_config: TestScenario1(env_config))
 
-# # Create our RLlib Trainer.
-# algo = PPO(config=config)
+    # Configure the algorithm.
+    config = {
+        # Environment (RLlib understands openAI gym registered strings).
+        "env": env_name,  #"Taxi-v3",
+        "env_config": env_config,
+        # Use 2 environment workers (aka "rollout workers") that parallelly
+        # collect samples from their own environment clone(s).
+        "num_workers": 1,  #2,
+        # Change this to "framework: torch", if you are using PyTorch.
+        # Also, use "framework: tf2" for tf2.x eager execution.
+        "framework": "tf",
+        # Tweak the default model provided automatically by RLlib,
+        # given the environment's observation- and action spaces.
+        "model": {
+            "fcnet_hiddens": [64, 64],
+            "fcnet_activation": "relu",
+        },
+        # Set up a separate evaluation worker set for the
+        # `algo.evaluate()` call after training (see below).
+        # "evaluation_num_workers": 1,
 
-# # Run it for n training iterations. A training iteration includes
-# # parallel sample collection by the environment workers as well as
-# # loss calculation on the collected batch and a model update.
-# for _ in range(3):
-#     print(algo.train())
+        "horizon": env_config["max_timesteps"],
 
-# # Evaluate the trained Trainer (and render each timestep to the shell's
-# # output).
+        # Only for evaluation runs, render the env.
+        "evaluation_config": {
+            "render_env": True,
+        },
+    }
+
+    # Create our RLlib Trainer.
+    algo = PPO(config=config)
+
+    # Run it for n training iterations. A training iteration includes
+    # parallel sample collection by the environment workers as well as
+    # loss calculation on the collected batch and a model update.
+    for _ in range(1):
+        print(algo.train())
+
+# Evaluate the trained Trainer (and render each timestep to the shell's
+# output).
 # algo.evaluate()
+
+if __name__ == "__main__":
+    train()
