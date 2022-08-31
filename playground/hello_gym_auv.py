47d634c41a3c5fe85fac97303951edfcedf51c45
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-08-31 11:28:38 +0200

    Ran first experiment with RLlib and gym-auv

--- playground/hello_gym_auv.py
+++ playground/hello_gym_auv.py
@@ -6,8 +6,9 @@
 
 import gym_auv
 import gym
-env_name = "TestScenario1-v0" 
-env_config = gym_auv.SCENARIOS[env_name]['config'].copy()
+
+env_name = "TestScenario1-v0"
+env_config = gym_auv.SCENARIOS[env_name]["config"].copy()
 print(f"{env_config = }")
 env = gym.make(env_name, env_config=env_config)
 # Import the RL algorithm (Algorithm) we would like to use.
@@ -20,6 +21,7 @@ env.reset()
 obs = env.step([0.5, 0.5])
 print(obs)
 
+
 def train():
     # Register environment in RLlib
     tune.register_env(env_name, lambda env_config: TestScenario1(env_config))
@@ -27,11 +29,11 @@ def train():
     # Configure the algorithm.
     config = {
         # Environment (RLlib understands openAI gym registered strings).
-        "env": env_name,  #"Taxi-v3",
+        "env": env_name,  # "Taxi-v3",
         "env_config": env_config,
         # Use 2 environment workers (aka "rollout workers") that parallelly
         # collect samples from their own environment clone(s).
-        "num_workers": 1,  #2,
+        "num_workers": 1,  # 2,
         # Change this to "framework: torch", if you are using PyTorch.
         # Also, use "framework: tf2" for tf2.x eager execution.
         "framework": "tf",
@@ -44,9 +46,7 @@ def train():
         # Set up a separate evaluation worker set for the
         # `algo.evaluate()` call after training (see below).
         # "evaluation_num_workers": 1,
-
         "horizon": env_config["max_timesteps"],
-
         # Only for evaluation runs, render the env.
         "evaluation_config": {
             "render_env": True,
@@ -61,6 +61,7 @@ def train():
     # loss calculation on the collected batch and a model update.
     for _ in range(1):
         print(algo.train())
+
 
 # Evaluate the trained Trainer (and render each timestep to the shell's
 # output).
