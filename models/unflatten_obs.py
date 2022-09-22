e49b480f2f541c4f350a6246d96e5220b63ec0be
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-09-22 13:42:07 +0200

    Added functionality for unflattening observations

---
+++ models/unflatten_obs.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+from torch import TensorType
+
+
+def unflatten_obs(
+    flat_obs, nav_shape=(6,), lidar_shape=(3, 180)
+) -> Tuple[TensorType, TensorType]:
+    # Assumes observation of size [B, N],
+    # Where B is batch size and N is the number of individual observations
+
+    n_nav_obs = nav_shape[0]
+    nav_obs = flat_obs[:, :n_nav_obs]
+    lidar_obs = flat_obs[:, n_nav_obs:].reshape(-1, *lidar_shape)
+
+    return nav_obs, lidar_obs
