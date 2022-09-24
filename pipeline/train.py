d9e05a946a5d9cb5ce6977d57df17e3118d3fe68
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-09-24 15:08:25 +0200

    Added gpu flag

--- pipeline/train.py
+++ pipeline/train.py
@@ -1,7 +1,10 @@
+import argparse
 from ray import tune
 
 from pipeline.register_envs import register_gym_auv_scenarios
-from models.auv_dreamer import auv_dreamer_factory, get_auv_dreamer_config
+from models.auv_dreamer import AuvDreamer, auv_dreamer_factory, get_auv_dreamer_config
+
+from ray.rllib.algorithms.dreamer.dreamer import DreamerConfig
 
 
 def main():
@@ -9,7 +12,19 @@ def main():
     register_gym_auv_scenarios()
 
     env_name = "MovingObstaclesNoRules-v0"
-    auv_dreamer = auv_dreamer_factory(env_name)
+    # auv_dreamer = auv_dreamer_factory(env_name)
+    dreamer_config = get_auv_dreamer_config()
+
+    assert isinstance(dreamer_config, DreamerConfig)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--gpu", action="store_true")
+    args = parser.parse_args()
+
+    if args.gpu:
+        dreamer_config.resources(num_gpus=1)
+
+    auv_dreamer = AuvDreamer(dreamer_config)
 
     for i in range(10):
         auv_dreamer.train()
