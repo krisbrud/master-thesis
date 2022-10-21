9fe5e5a9824b830264c470952bcf2df105a164d5
Kristian Brudeli <krisbrud@stud.ntnu.no>
2022-10-21 11:52:04 +0000

    First occupancy grid made

--- playground/occupancy_grid.py
+++ playground/occupancy_grid.py
@@ -4,24 +4,27 @@ GRID_SIDE_SIZE = 64  # Height/width of grid
 N_SENSORS = 180
 SENSOR_RANGE = 150
 
-
-sensor_angles = (np.arange(N_SENSORS) * 2 * np.pi) - np.pi
+sensor_angles = (np.arange(N_SENSORS) * 2 / np.pi) - np.pi
 print(f"{sensor_angles[0] = }")
 print(f"{sensor_angles[-1] = }")
 
 ranges = np.linspace(0, SENSOR_RANGE, N_SENSORS)
 print(f"{ranges = }")
 
-occupancy_grid = np.array((GRID_SIDE_SIZE, GRID_SIDE_SIZE))
-# x_pos = ranges * np.cos(sensor_angles) 
-# y_pos = ranges * np.sin(sensor_angles)
+occupancy_grid = np.zeros((GRID_SIDE_SIZE, GRID_SIDE_SIZE))
 
-pos = np.vstack([np.cos(sensor_angles), np.sin(sensor_angles)]).T * ranges  # Each row are coordinates of a ray
+pos = (
+    np.vstack([ranges * np.cos(sensor_angles), ranges * np.sin(sensor_angles)])
+).T  # Each row are coordinates of a ray
 print(f"{pos = }")
 
-indices = pos // np.array([GRID_SIDE_SIZE / 2, GRID_SIDE_SIZE/ 2])
+collisions = np.array([True] * N_SENSORS)
+
+indices_decimals = (pos * (GRID_SIDE_SIZE / 2) / SENSOR_RANGE) + (GRID_SIDE_SIZE / 2)
+indices = np.floor(indices_decimals).astype(np.int32)
+indices_with_collisions = indices[collisions, :]
 print(f"{indices = }")
 
-occupancy_grid[indices] = 1.0
+occupancy_grid[indices[:, 0], indices[:, 1]] = 1.0
 
-print(occupancy_grid)
+print(f"{np.mean(occupancy_grid) = }")
