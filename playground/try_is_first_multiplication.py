13c412837557015ce7c4c07df2627606af136197
Kristian Brudeli <krisbrud@stud.ntnu.no>
2023-01-18 20:57:14 +0000

    Fix is first multiplication

---
+++ playground/try_is_first_multiplication.py
@@ -0,0 +1,24 @@
+# %%
+import torch
+
+batch_size = 10
+deter_size = 15
+stoch_size = 5
+states = [torch.ones((batch_size, deter_size)), torch.ones((batch_size, stoch_size))]
+
+states
+
+is_first = torch.zeros((batch_size,))
+is_first[3] = 1
+is_not_first = 1 - is_first
+
+is_not_first
+
+for state in states:
+    state_out = is_not_first.reshape(1, -1)
+    print(state_out)
+
+
+states
+
+# %%
