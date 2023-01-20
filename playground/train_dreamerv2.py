fa6d246469010cd13ba0ecc5fc29523638f2aac8
Kristian Brudeli <krisbrud@stud.ntnu.no>
2023-01-20 18:42:40 +0000

    Trying to revert dreamer to jan 16-4 experiment

--- playground/train_dreamerv2.py
+++ playground/train_dreamerv2.py
@@ -7,15 +7,15 @@ import dreamerv2.api as dv2
 
 import copy
 
-log_suffix = "-jan-20-6"
+log_suffix = "-jan-20-9"
 # Override some of the default config options located in dreamerv2/configs.yaml
 config = dv2.defaults.update({
     ## Training options
     "logdir": "~/repos/master-thesis/logdir/MovingObstaclesLosRewarder-v0" + log_suffix,
     "log_every": 1e3,
     "train_every": 10,
-    "eval_every": 5e3, # Doesn't really do anything but write to tensorboard
-    "prefill": 1e3, #5e4,
+    "eval_every": 10e3, # Doesn't really do anything but write to tensorboard
+    "prefill": 50e3, #5e4,
     # "envs": 4, 
 
     ## Agent options
@@ -24,13 +24,13 @@ config = dv2.defaults.update({
     "actor_ent": 1e-4,
     
     ## Model options
-    "loss_scales.kl": 0.1, # 1.0,
+    "loss_scales.kl": 1.0,
     "discount": 0.995,  # 0.99 as default
     # "kl": {"free": 1.0}, 
-    "kl": {"free": 0.1}, # No free bits, like in atari envs
-    "model_opt": {"lr": 3e-4},
-    "actor_opt": {"lr": 8e-5},
-    "critic_opt": {"lr": 8e-5},
+    "kl": {"free": 1.0}, 
+    "model_opt": {"lr": 1e-5},
+    "actor_opt": {"lr": 1e-5},
+    "critic_opt": {"lr": 1e-5},
     "rssm": {"deter": 200, "hidden": 200}, 
     "encoder": {"mlp_keys": "dense", "cnn_keys": "image"},
     "decoder": {"mlp_keys": "dense", "cnn_keys": "image"},
@@ -39,7 +39,7 @@ config = dv2.defaults.update({
 gym_auv_config = gym_auv.LOS_COLAV_CONFIG
 gym_auv_config.sensor.use_image_observation = True
 gym_auv_config.episode.use_terminated_truncated_step_api = True
-gym_auv_config.sensor.n_lidar_rays = 90
+# gym_auv_config.sensor.n_lidar_rays = 90
 
 env_name = "MovingObstaclesLosRewarder-v0"
 env = gym.make(env_name, env_config=gym_auv_config)
